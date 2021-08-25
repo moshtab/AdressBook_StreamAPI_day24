3d3b9328846240cab4a8d8eed5efb64d08267e41
moshtab <mdmohsin41439@gmail.com>
2021-08-25 17:09:02 +0530

    added msg

---
+++ AdressBook.java
@@ -0,0 +1,10 @@
+package adressbook.streamAPI;
+
+public class AdressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address book computation program");
+
+	}
+
+}
