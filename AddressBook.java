b8f5572a734b723db75d654a00e1c90fdf1e7e10
moshtab <mdmohsin41439@gmail.com>
2021-08-26 20:56:40 +0530

    Edited contact1 by using set() method

--- AddressBook.java
+++ AddressBook.java
@@ -19,13 +19,47 @@ public class AddressBook {
 		AddressBook adressBook = new AddressBook(list);
 
 		// Adding contact1 in addressBook
+		System.out.println("Displaying contact1 details");
 		adressBook.addContact();
 		// Adding contact2 in addressBook
+		System.out.println("Displaying contact2 details");
 		adressBook.addContact();
+		// Editing contact1
+		System.out.println("Editing Contact1 details");
+		list.set(0, adressBook.editContact());
 
 		// using stream to list the elements
 		List<Contact> result = list.stream().collect(Collectors.toList());
 		System.out.println(result);
+
+	}
+
+	private Contact editContact() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the First name");
+		String firstName = s.nextLine();
+
+		System.out.println("Enter the Last name");
+		String lastName = s.nextLine();
+
+		System.out.println("Enter the City name");
+		String city = s.nextLine();
+
+		System.out.println("Enter the State name");
+		String state = s.nextLine();
+
+		System.out.println("Enter the email");
+		String email = s.nextLine();
+
+		System.out.println("Enter the Phone number");
+		String phoneNumber = s.nextLine();
+
+		System.out.println("Enter the Zip");
+		String zip = s.nextLine();
+		Contact contact = new Contact(firstName, lastName, city, state, email, phoneNumber, zip);
+
+		list.add(contact);
+		return contact;
 
 	}
 
